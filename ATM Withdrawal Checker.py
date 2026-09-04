31403aff41ca8f70f172e7ff3532012d7ff6cf50
Devu9026 <ritua0662@gmail.com>
2026-09-04 12:19:39 +0530

    Update ATM Withdrawal Checker.py

--- ATM Withdrawal Checker.py
+++ ATM Withdrawal Checker.py
@@ -3,7 +3,9 @@ print("your Balance is:", balance)
 print("enter amount in multiple of 100's form:")
 amount=int(input("enter your Withdrawl amount:"))
 check=amount%100
-if(amount>0 and check==0):
+if(amount<0):
+    print("enter correct withdrawl amount")
+elif(amount<=balance and check==0):
     print("Accepted")
 else:
     print("insufficient Balance or not multiple of 100")
